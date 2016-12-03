30b8f86832ed059b7b1be533c09115b90bc4cea5
Kracekumar R <me@kracekumar.com>
2016-12-03 18:35:51 -0500

    Implement basic udp echo server

--- src/edht_sup.erl
+++ src/edht_sup.erl
@@ -28,8 +28,38 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
+    io:format('Starting EDHT~n'),
+    Port = get_port(),
+    listen_to_clients(Port),
     {ok, { {one_for_all, 0, 1}, []} }.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+get_port() ->
+    case os:getenv("PORT") of
+        false ->
+            4545;
+        Value -> list_to_integer(Value)
+    end.
+
+
+listen_to_clients(Port) ->
+    %% Listen to all UDP clients.
+    spawn_link(fun() -> handle_client_connections(Port) end).
+
+
+handle_client_connections(Port) ->
+    {ok, Socket} = gen_udp:open(Port, [binary, {active, false}]),
+    io:format("Listening to port ~p~n", [Port]),
+    client_loop(Socket).
+
+client_loop(Socket) ->
+    inet:setopts(Socket, [{active, once}]),
+    receive
+        {udp, Socket, Host, Port, Bin} ->
+            io:format("Received data `~p` `~p` from `~p`~n", [Host, Port, Bin]),
+            % Spawn new Proc to send data back?
+            gen_udp:send(Socket, Host, Port, Bin),
+            client_loop(Socket)
+    end.
