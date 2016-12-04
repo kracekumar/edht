119ecd80f73e482a788be85a1e8659193991562e
Kracekumar R <me@kracekumar.com>
2016-12-03 21:47:53 -0500

    Add initial protype for client and server interaction

--- src/edht_sup.erl
+++ src/edht_sup.erl
@@ -9,6 +9,7 @@
 
 %% API
 -export([start_link/0]).
+-export([client/3]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -29,6 +30,8 @@ start_link() ->
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
     io:format('Starting EDHT~n'),
+    io:format("~p~n", [protobuffs:encode(1, 1, uint32)]),
+    protobuffs_compile:scan_file("src/request.proto"),
     Port = get_port(),
     listen_to_clients(Port),
     {ok, { {one_for_all, 0, 1}, []} }.
@@ -59,7 +62,38 @@ client_loop(Socket) ->
     receive
         {udp, Socket, Host, Port, Bin} ->
             io:format("Received data `~p` `~p` from `~p`~n", [Host, Port, Bin]),
+            {_, Method, Key, Value} = request_pb:decode_request(Bin),
+            spawn_link(fun() -> handle_request(Socket, Host, Port, Method, Key, Value) end),
             % Spawn new Proc to send data back?
-            gen_udp:send(Socket, Host, Port, Bin),
             client_loop(Socket)
     end.
+
+
+handle_request(Socket, Host, Port, Method, Key, Value) ->
+    io:format("Key: ~p, Value: ~p, Method: ~p~n",[Key, Value, Method]),
+    if 
+        Method =:= "GET" -> spawn_link(fun() -> handle_get(Socket, Host, Port, Key) end);
+        Method =:= "PUT" -> spawn_link(fun() -> handle_put(Socket, Host, Port, Key, Value) end);
+        true -> spawn_link(fun() -> handle_rogue_msg(Socket, Host, Port, Method, Key, Value) end)
+    end.
+        
+handle_rogue_msg(Socket, Host, Port, Method, Key, Value) ->
+    io:format('rogue'),
+    gen_udp:send(Socket, Host, Port, request_pb:encode({request, Method, Key, Value})).
+
+
+handle_get(Socket, Host, Port, Key) ->
+    io:format("Key: ~p~n", [Key]),
+    gen_udp:send(Socket, Host, Port, request_pb:encode({request, "GET", Key, undefined})).
+
+handle_put(Socket, Host, Port, Key, Value) ->
+    io:format("Key: ~p, Value: ~p ~n", [Key, Value]),
+    gen_udp:send(Socket, Host, Port, request_pb:encode({request, "GET", Key, Value})).
+
+
+client(Method, Key, Value) ->
+    {ok, Socket} = gen_udp:open(8888),
+    gen_udp:send(Socket, {127,0,0,1}, 4545, request_pb:encode(
+                                              {request, Method, Key, Value})),
+    gen_udp:close(Socket).
+       
