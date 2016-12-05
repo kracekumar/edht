99a13a8f40b647c0b91e1610e5da9a527d334586
Geoff Anderson <granders@gmail.com>
2016-12-05 18:50:48 -0500

    Added bitcask integration

--- src/edht_sup.erl
+++ src/edht_sup.erl
@@ -38,6 +38,11 @@ init([]) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+-define(DATADIR, ".bitcaskdata").
+
+open_store() ->
+    bitcask:open(?DATADIR, [read_write]).
+
 get_port() ->
     case os:getenv("PORT") of
         false ->
@@ -56,17 +61,26 @@ handle_client_connections(Port) ->
     io:format("Listening to port ~p~n", [Port]),
     client_loop(Socket).
 
+to_binary(Val) when is_binary(Val) ->
+    Val;
+to_binary(Val) when is_atom(Val) ->
+    Val;
+to_binary(Val) when is_integer(Val) ->
+    integer_to_binary(Val);
+to_binary(Val) ->
+    list_to_binary(Val).
+
 client_loop(Socket) ->
     inet:setopts(Socket, [{active, once}]),
+
     receive
         {udp, Socket, Host, Port, Bin} ->
             io:format("Received data `~p` `~p` from `~p`~n", [Host, Port, Bin]),
             {_, Method, Key, Value} = request_pb:decode_request(Bin),
-            spawn_link(fun() -> handle_request(Socket, Host, Port, Method, Key, Value) end),
+            spawn_link(fun() -> handle_request(Socket, Host, Port, Method, to_binary(Key), to_binary(Value)) end),
             % Spawn new Proc to send data back?
             client_loop(Socket)
     end.
-
 
 handle_request(Socket, Host, Port, Method, Key, Value) ->
     io:format("Key: ~p, Value: ~p, Method: ~p~n",[Key, Value, Method]),
@@ -82,12 +96,27 @@ handle_rogue_msg(Socket, Host, Port, Method, Key, Value) ->
 
 
 handle_get(Socket, Host, Port, Key) ->
-    io:format("Key: ~p~n", [Key]),
-    gen_udp:send(Socket, Host, Port, request_pb:encode({request, "GET", Key, undefined})).
+    DBHandle = open_store(),
+    case bitcask:get(DBHandle, Key) of
+        {ok, Value} ->
+            io:format("Key: ~p Value: ~p~n", [Key, Value]),
+            gen_udp:send(Socket, Host, Port, request_pb:encode({request, "GET", Key, Value}));
+
+        not_found ->
+            io:format("Key: ~p Value: ~p~n", [Key, not_found]),
+            gen_udp:send(Socket, Host, Port, request_pb:encode({request, "GET", Key, undefined}))
+    end.
 
 handle_put(Socket, Host, Port, Key, Value) ->
-    io:format("Key: ~p, Value: ~p ~n", [Key, Value]),
-    gen_udp:send(Socket, Host, Port, request_pb:encode({request, "GET", Key, Value})).
+    DBHandle = open_store(),
+    case bitcask:put(DBHandle, Key, Value) of
+        ok ->
+            gen_udp:send(Socket, Host, Port, request_pb:encode({request, "PUT", Key, Value})),
+            io:format("Key: ~p, Value: ~p ~n", [Key, Value]);
+        {error, _} ->
+            gen_udp:send(Socket, Host, Port, request_pb:encode({request, "PUT", Key, error})),
+            io:format("error: Key: ~p, Value: ~p ~n", [Key, Value])
+    end.
 
 
 client(Method, Key, Value) ->
